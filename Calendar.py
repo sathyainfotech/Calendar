e0e397ae94346f58f53dd0f946f27a738ed4f25a
sathyainfotech <84429523+sathyainfotech@users.noreply.github.com>
2021-06-17 20:06:46 -0700

    File Upload

---
+++ Calendar.py
@@ -0,0 +1,10 @@
+import calendar
+
+year=int(input("Enter The Year:"))
+# month=int(input("Enter The Month:"))
+print("\n")
+# n=12
+# for i in range(1,n+1):
+#     print(calendar.month(year,i))
+
+print(calendar.calendar(year))
